500d6c8669d716766ab04056ae5dfb2a14ee2451
Gagan <103169116+gagandeep-gingh-bt@users.noreply.github.com>
2022-09-28 11:01:06 +0530

    v0.0.2: Expose express checkout functionality as per grouped.

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,6 @@
 //@ Description: Exports default functionality.
 */
 
-import { Card } from "./express-checkout/card";
-import { Customer } from "./express-checkout/customer";
-import { CustomerList } from "./express-checkout/customer-list";
-import { Order } from "./express-checkout/order";
-import { OrderList } from "./express-checkout/order-list";
-import { Payment } from "./express-checkout/payment";
-import { PaymentMethod } from "./express-checkout/payment-method";
-import { PaymentLinks } from "./express-checkout/payment-links";
-import { Refund } from "./express-checkout/refund";
-import { Wallet } from "./express-checkout/wallet";
-import { WalletList } from "./express-checkout/wallet-list";
+import * as ExpressCheckout from "./express-checkout";
 
-export { Card, Customer, CustomerList, Order, OrderList, Payment, Refund, Wallet, WalletList, PaymentLinks, PaymentMethod };
+export { ExpressCheckout };
